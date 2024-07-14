74f2a4ce98439074fa238001b9ec644601a49b13
ravel01106 <ravello2002@gmail.com>
2024-07-14 18:32:24 +0100

    refactor: rename entity package to model package

--- src/main/java/com/aravelo/passwordvalidator/app/models/Validator.java
+++ src/main/java/com/aravelo/passwordvalidator/app/models/Validator.java
@@ -1,4 +1,4 @@
-package com.aravelo.passwordvalidator.app.entities;
+package com.aravelo.passwordvalidator.app.models;
 
 public class Validator {
 
@@ -8,7 +8,9 @@ public class Validator {
   private boolean containsNumber;
   private boolean containUnderscore;
 
-  public Validator(){}
+  public Validator(){
+
+  }
 
   public Validator(Integer lenghtValid, boolean containsUpperCase, boolean containsLowerCase, boolean containsNumber,
       boolean containUnderscore) {
@@ -18,37 +20,45 @@ public class Validator {
     this.containsNumber = containsNumber;
     this.containUnderscore = containUnderscore;
   }
+
   public Integer getLenghtValid() {
     return lenghtValid;
   }
+
   public void setLenghtValid(Integer lenghtValid) {
     this.lenghtValid = lenghtValid;
   }
+
   public boolean isContainsUpperCase() {
     return containsUpperCase;
   }
+
   public void setContainsUpperCase(boolean containsUpperCase) {
     this.containsUpperCase = containsUpperCase;
   }
+
   public boolean isContainsLowerCase() {
     return containsLowerCase;
   }
+
   public void setContainsLowerCase(boolean containsLowerCase) {
     this.containsLowerCase = containsLowerCase;
   }
+
   public boolean isContainsNumber() {
     return containsNumber;
   }
+
   public void setContainsNumber(boolean containsNumber) {
     this.containsNumber = containsNumber;
   }
+
   public boolean isContainUnderscore() {
     return containUnderscore;
   }
+
   public void setContainUnderscore(boolean containUnderscore) {
     this.containUnderscore = containUnderscore;
   }
-
-
 
 }
